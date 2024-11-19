476c326e8794a0f6eb4d725d720a8c4367674fbb
Tataata13 <emili.edel.ten@gmail.com>
2024-11-20 08:46:54 +0900

    modify banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,38 +9,9 @@ export const Banner = () => {
     const [isDeleting, setIsDeleting]= useState(false);
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const [text, setText] = useState('');
-    /*const toRotate = ["Natalia", "Web Developer"];*/
     const period = 2000;
     const toRotate = useMemo(() => ["Natalia", "Web Developer"], []);
-   /* useEffect(() => {
-        let ticker = setInterval(() => {
-            tick();
-        }, delta);
 
-        return () => {
-            clearInterval(ticker)};
-        }, [text])
-
-        const tick = () => {
-        let i = loopNum % toRotate.length;
-        let fullText = toRotate[i];
-        let updateText = isDeleting ? fullText.substring(0, text.length -1) : fullText.substring(0, text.length +1);
-        
-        setText(updateText);
-
-        if (isDeleting) {
-            setDelta(prevDelta => prevDelta /2)
-        }
-        if (!isDeleting && updateText === fullText) {
-            setIsDeleting(true);
-            setDelta(period);
-        } else if (isDeleting && updateText === '') {
-            setIsDeleting(false);
-            setLoopNum(loopNum +1);
-            setDelta(500);
-        }
-    }
-    */
     const tick = useCallback(() => {
         let i = loopNum % toRotate.length;
         let fullText = toRotate[i];
